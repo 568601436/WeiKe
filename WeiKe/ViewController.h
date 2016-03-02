555a7019deed0574e59f0edc197d69b567ae6904
568601436 <568601436@qq.com>
2016-03-02 16:22:30 +0800

    111111

---
+++ WeiKe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiKe
+//
+//  Created by OSellMobile on 16/3/2.
+//  Copyright © 2016年 CGMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeiKe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeiKe
+//
+//  Created by OSellMobile on 16/3/2.
+//  Copyright © 2016年 CGMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
